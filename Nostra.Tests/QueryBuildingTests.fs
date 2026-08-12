0a10f8632aac9d5c0bf0290fe15a341a9320a0ad
Lucas Ontivero <lucasontivero@gmail.com>
2026-08-12 01:40:24 -0300

    Fix LIMIT in subscriptions with many filters

--- Nostra.Tests/QueryBuildingTests.fs
+++ Nostra.Tests/QueryBuildingTests.fs
@@ -19,8 +19,8 @@ let createFilter s =
     | Ok filter -> filter
 
 let materializeSingleQuery (query: Database.Query) =
-    let s, e, _ = Database.materializeQuery defaultLimit maxLimit query 0
-    s, e    
+    let baseQuery, limitStr, e, _ = Database.materializeQuery defaultLimit maxLimit query 0
+    baseQuery + limitStr, e
 
 type ``Single Filters``(output:ITestOutputHelper) =
         
@@ -164,7 +164,7 @@ type ``Subscriptions (multiple Filters)``(output:ITestOutputHelper) =
         let filter1 = createFilter "{ \"kinds\" : [1,2], \"#e\": [\"888888\"] }"
         let filter2 = createFilter "{ \"kinds\" : [3], \"limit\": 123 }"
         let query, parameters = Database.buildQueryForFilters [filter1; filter2] defaultLimit maxLimit now
-        should equal "SELECT e.serialized_event FROM events e WHERE e.deleted = @s0_e_deleted AND e.expires_at > @s0_e_expires_at AND e.kind IN (@s0_e_kind0,@s0_e_kind1) AND e.id IN (SELECT t.event_id FROM tags t WHERE t.name = @s1_t_name AND t.value IN (@s1_t_value0) AND t.kind IN (@s1_t_kind0,@s1_t_kind1) ORDER BY e.created_at DESC, e.id DESC LIMIT 50) ORDER BY e.created_at DESC, e.id DESC LIMIT 50 UNION SELECT e.serialized_event FROM events e WHERE e.deleted = @s2_e_deleted AND e.expires_at > @s2_e_expires_at AND e.kind IN (@s2_e_kind0) ORDER BY e.created_at DESC, e.id DESC LIMIT 123" query
+        should equal "SELECT e.serialized_event FROM events e WHERE e.deleted = @s0_e_deleted AND e.expires_at > @s0_e_expires_at AND e.kind IN (@s0_e_kind0,@s0_e_kind1) AND e.id IN (SELECT t.event_id FROM tags t WHERE t.name = @s1_t_name AND t.value IN (@s1_t_value0) AND t.kind IN (@s1_t_kind0,@s1_t_kind1) ORDER BY e.created_at DESC, e.id DESC LIMIT 50) ORDER BY e.created_at DESC, e.id DESC LIMIT 50 UNION SELECT * FROM (SELECT e.serialized_event FROM events e WHERE e.deleted = @s2_e_deleted AND e.expires_at > @s2_e_expires_at AND e.kind IN (@s2_e_kind0) ORDER BY e.created_at DESC, e.id DESC LIMIT 123)" query
         should equal [
            "@s0_e_deleted", false :> obj
            "@s0_e_expires_at", now :> obj
